fba1fdb81060b33de2c7a6e7a3108f7010640e53
Vikas <vikascjadhav@gmail.com>
2018-11-19 09:26:26 +0530

    Encode Data

--- ml.py
+++ ml.py
@@ -1,21 +1,34 @@
+
 import pandas as pd 
 
-data = pd.read_pickle('trainingData1.pkl',compression="zip")
-print(data.head())
-print(data.columns)
-values =data.output_closeAuctionVolume
-""" 
-['date', 'stock', 'binNum', 'binStartTime', 'binEndTime',
-       'auctionIndicator', 'volume', 'binStartPrice', 'binEndPrice',
-       'day_openPrice', 'day_closePrice', 'day_lowPrice', 'day_highPrice',
-       'output_remainingVolume', 'output_closeAuctionVolume',
-       'output_closePriceDirection'],
-        """
+"""
+Attribute from data given for learning
 
-print(values.head())
+[
+ 'date', 'stock', 'binNum', 'binStartTime', 'binEndTime','auctionIndicator',
+ 'volume', 'binStartPrice', 'binEndPrice','day_openPrice', 'day_closePrice', 'day_lowPrice',
+ 'day_highPrice', 'output_remainingVolume', 'output_closeAuctionVolume',
+ 'output_closePriceDirection'
+ ],
+"""
 
-import matplotlib.pyplot as plt
+
+
+file_data = pd.read_pickle('trainingData1.pkl',compression="zip")
+
+print(file_data.binNum)
+""" import matplotlib.pyplot as plt
 y = data.binStartTime
 x = data.date
 plt.scatter(x, y)
-plt.show()  # or plt.savefig("name.png")
+plt.show()  """
+
+data = file_data.drop(['output_remainingVolume','output_closeAuctionVolume','output_closePriceDirection'], 1)
+rvolume_labels =  file_data['output_remainingVolume']
+
+from sklearn.preprocessing import MinMaxScaler
+scalarX, scalarY = MinMaxScaler(), MinMaxScaler()
+
+y = scalarY.fit_transform(rvolume_labels.values.reshape(-1,1))
+
+print("Completed")
